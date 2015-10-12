d9621683442bdfdeac1fdcc7de5250a9a9e54be5
timower <timothy@werquin.com>
2015-10-12 13:38:17 +0200

    Added basic login logic in comments

--- app/src/main/java/org/peno/b4/bikerisk/MainActivity.java
+++ app/src/main/java/org/peno/b4/bikerisk/MainActivity.java
@@ -1,5 +1,6 @@
 package org.peno.b4.bikerisk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,7 +8,29 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //LoginManager mLoginManager;
+
         super.onCreate(savedInstanceState);
+//
+//         if (!mLoginManager.checkLogin()) {
+//              create loginActivity intent
+//              start intent for result
+//              get key from intent
+//              if still not logged in -> stop / error
+//          }
+
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (requestCode == LOGIN_REQUEST) {
+//            if (resultCode == RESULT_OK) {
+//                // get key from data
+//                // done
+//            } else {
+//
+//            }
+//        }
     }
 }
