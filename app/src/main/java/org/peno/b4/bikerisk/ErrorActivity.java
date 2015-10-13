a9f9f8e6bdf518e4045419bce625eb7eca5d6c81
Timothy Werquin <Timothy@werquin.com>
2015-10-13 17:52:47 +0200

    Add error activityy

---
+++ app/src/main/java/org/peno/b4/bikerisk/ErrorActivity.java
@@ -0,0 +1,13 @@
+package org.peno.b4.bikerisk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ErrorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_error);
+    }
+}
