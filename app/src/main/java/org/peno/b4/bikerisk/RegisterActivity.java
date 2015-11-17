9c6430ad8bb5c6b0e090075cc88b95b8b3aa1d38
Jan Vermaelen <vermaelen_jan@hotmail.com>
2015-11-17 19:44:36 +0100

    register functions

--- app/src/main/java/org/peno/b4/bikerisk/RegisterActivity.java
+++ app/src/main/java/org/peno/b4/bikerisk/RegisterActivity.java
@@ -47,10 +47,12 @@ public class RegisterActivity extends AppCompatActivity
 
         View color = findViewById(R.id.colorView);
         int colorCode = ((ColorDrawable)color.getBackground()).getColor();
-        if (name.equals("") || pass.equals("") || email.equals(""))
-            return;
-        connectionManager.createUser(name, pass, email, colorCode);
-        //create account with username, password, email and color
+        if (name.equals("")) {Toast.makeText(this, "Please fill in a name!", Toast.LENGTH_SHORT).show();}
+        else if (pass.equals("")) {Toast.makeText(this, "Please fill in a password!", Toast.LENGTH_SHORT).show();}
+        else if (email.equals("")) {Toast.makeText(this, "Please fill in an email address!", Toast.LENGTH_SHORT).show();}
+        else if (colorCode == Color.rgb(0,0,0)) {Toast.makeText(this, "Please choose a color!", Toast.LENGTH_SHORT).show();}
+        else connectionManager.createUser(name, pass, email, colorCode);
+              //create account with username, password, email and color
     }
 
     @Override
