edd46da95d45a1e7ed40a3cafaf2b75fe2238872
timower <timothy@werquin.com>
2015-12-05 14:56:50 +0100

    removed production ip & port from master

--- app/src/main/java/org/peno/b4/roadwars/Utils.java
+++ app/src/main/java/org/peno/b4/roadwars/Utils.java
@@ -24,8 +24,6 @@ import java.util.List;
 public class Utils {
     public static final float MPS_TO_KMH = 3.6f;
     public static final String HOST = "128.199.52.178";
-    //TODO: user production server:
-    //public static final String HOST = "128.199.56.127";
     public static final int PORT = 4444;
     public static final float MIN_ACCURACY = 18.0f;
 
