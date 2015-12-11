12d799bb00dbec165bc1794563e1e294ed5ae9e4
timower <timothy@werquin.com>
2015-12-11 21:01:43 +0100

    fix bug reported in play store

--- app/src/main/java/org/peno/b4/roadwars/PositionManager.java
+++ app/src/main/java/org/peno/b4/roadwars/PositionManager.java
@@ -155,7 +155,8 @@ public class PositionManager implements LocationListener {
         @Override
         protected void onPostExecute(PolylineOptions res) {
             //TODO: check if errors:
-            pOptions = res;
+            if (res != null)
+                pOptions = res;
             if (res != null && UIobjects != null) {
                 userRoute.remove();
                 userRoute = UIobjects.mMap.addPolyline(res);
